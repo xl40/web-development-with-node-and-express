41595512471f91463f92bd21ec591941fa90f521
xuelin <353006063@qq.com>
2022-09-05 20:24:15 +0800

    在 404 处理器之前加上两个路由

--- _main/meadowlark.js
+++ _main/meadowlark.js
@@ -4,6 +4,16 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+app.get('/', function(req, res){
+    res.type('text/plain');
+    res.send('Meadowlark Travel');
+});
+app.get('/about', function(req, res){
+    res.type('text/plain');
+    res.send('About Meadowlark Travel');
+});
+
+
 // 定制404页面
 app.use(function (req, res) {
     res.type('text/plain');
