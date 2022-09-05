95148c5728f9806b759c8f6b488df1c11a0e30bf
xuelin <353006063@qq.com>
2022-09-05 20:04:13 +0800

    add serveStaticFile

--- _main/hello.js
+++ _main/hello.js
@@ -1,26 +1,41 @@
-let http = require('http');
+var http = require('http'),
+    fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if (!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function (err, data) {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('500 - Internal Error');
+        } else {
+            res.writeHead(responseCode,
+                {'Content-Type': contentType});
+            res.end(data);
+        }
+    });
+}
+
 
 http.createServer(function (req, res) {
-
-
-    let path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-
+    // 规范化url，去掉查询字符串、可选的反斜杠，并把它变成小写
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '')
+        .toLowerCase();
     switch (path) {
         case '':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('Homepage');
+            serveStaticFile(res, '/public/home.html', 'text/html');
             break;
         case '/about':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('About');
+            serveStaticFile(res, '/public/about.html', 'text/html');
+            break;
+        case '/img/logo.jpg':
+            serveStaticFile(res, '/public/img/logo.jpg',
+                'image/jpeg');
             break;
         default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Not Found');
+            serveStaticFile(res, '/public/404.html', 'text/html',
+                404);
             break;
     }
-
-
 }).listen(3000);
 
-console.log('Server started on localhost:3000; ... ')
+console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
