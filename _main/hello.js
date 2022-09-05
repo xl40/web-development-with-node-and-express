16dca374e01ba737c65c8366ec299574e15fb4c1
xuelin <353006063@qq.com>
2022-09-05 19:41:39 +0800

    add route

--- _main/hello.js
+++ _main/hello.js
@@ -1,9 +1,26 @@
 let http = require('http');
 
-http.createServer(
-    function (req,res) {
-        res.writeHead(200,{'Content-Type':'text/plain'});
-        res.end('Hello world!');
+http.createServer(function (req, res) {
+
+
+    let path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+
+    switch (path) {
+        case '':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Homepage');
+            break;
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('About');
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Not Found');
+            break;
+    }
+
+
 }).listen(3000);
 
 console.log('Server started on localhost:3000; ... ')
